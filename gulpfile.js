70f883e51c715babaa689ede7b7c1c7a2eea16b9
cpowers <cpowers@DELOITTE.com>
2016-02-02 16:01:35 -0500

    gulpfile is working

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var uglify = require('gulp-uglify');
+var ngmin = require('gulp-ngmin');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var usemin = require('gulp-usemin');
+var rev = require('gulp-rev');
+var rimraf = require('gulp-rimraf');
+var bs = require('browser-sync').create();
+
+var paths = {
+    scripts: ['app/**/*.js', '!app/bower_components/**/*.js'],
+    html: [
+    './app/**/*.html',
+    '!./app/index.html',
+    '!./app/bower_components/**/*.html'
+  ],
+    index: './app/index.html',
+    build: '././build/'
+}
+
+gulp.task('rimraf', function () {
+    gulp.src(paths.build, {
+            read: false
+        })
+        .pipe(rimraf());
+});
+
+gulp.task('copy', ['rimraf'], function () {
+    gulp.src(paths.html)
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('usemin', ['copy'], function () {
+    gulp.src(paths.index)
+        .pipe(usemin({
+            css: [minifyCss(), 'concat'],
+            js: [ngmin(), uglify()]
+        }))
+        .pipe(gulp.dest(paths.build))
+});
+
+gulp.task('build', ['usemin']);
+
+gulp.task('connect', function () {
+    connect.server({
+        root: 'app/'
+    });
+});
+
+gulp.task('bs', function () {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('default', ['connect']);
