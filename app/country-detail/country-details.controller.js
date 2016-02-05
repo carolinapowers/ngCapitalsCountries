e7f0aa651449b905e92ed541a788256be9f04320
cpowers <cpowers@DELOITTE.com>
2016-02-05 09:07:38 -0500

    refactoring from using another data call to using

--- app/country-detail/country-details.controller.js
+++ app/country-detail/country-details.controller.js
@@ -1,13 +1,24 @@
 angular.module('countryCapital')
     .controller('CountryDetailsCtrl', CountryDetailsCtrl);
 
-CountryDetailsCtrl.$inject = ['dataService', '$scope', 'countryDetails', 'neighbors', 'oneCountry'];
+CountryDetailsCtrl.$inject = ['dataService', '$scope', 'countryDetails', 'neighbors', 'oneCountry', '$routeParams'];
 
-function CountryDetailsCtrl(dataService, $scope, countryDetails, neighbors, oneCountry) {
+function CountryDetailsCtrl(dataService, $scope, countryDetails, neighbors, oneCountry, $routeParams) {
 
 
     $scope.oneCountry = oneCountry.data.geonames[0];
 
+    $scope.countryParam = $routeParams.country;
+    console.log($scope.countryParam);
+
+    dataService.getCountries($routeParams.country).then(function (response) {
+        for (var i = 0; i < response.length; i++) {
+            $scope.countryDetail = response[i];
+        }
+
+
+
+    })
 
     for (var i = 0; i < $scope.oneCountry; i++) {
         //        return {
@@ -20,7 +31,7 @@ function CountryDetailsCtrl(dataService, $scope, countryDetails, neighbors, oneC
     $scope.country = countryDetails.data.geonames;
     console.log($scope.country);
 
-    //neighbors 
+    //neighbors
 
     $scope.neighbors = [];
     var vizinhos = neighbors.data.geonames;
